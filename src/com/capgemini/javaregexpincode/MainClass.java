5cd55a285f9772da3bedfca289eb79f7a94783a8
mamudsamiul <samiulmamud1995@gmail.com>
2020-09-28 16:42:03 +0530

    (Samiul Mamud) Added: Logic for 6 digit pincode is added

--- src/com/capgemini/javaregexpincode/MainClass.java
+++ src/com/capgemini/javaregexpincode/MainClass.java
@@ -1,15 +1,12 @@
 package com.capgemini.javaregexpincode;
 
 public class MainClass {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Pin Code validator");
-		String pinCode=TakingInput.read();
-		if(CheckFormat.check(pinCode))
+		String pinCode = TakingInput.read();
+		if (CheckFormat.check(pinCode))
 			System.out.println("Valid pincode");
 		else
 			System.out.println("Invalid pin code");
-
 	}
-
 }
